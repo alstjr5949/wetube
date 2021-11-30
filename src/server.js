feda75b626f27a657e2d74884cf0ca8db5183693
alstjr5949 <alstjr5949@gmail.com>
2021-11-30 23:38:25 +0900

    wetube 7.11 ~ 7.15

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import morgan from "morgan";
-import session from 'express-session';
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import { localsMiddleware } from './middlewares';
+import { localsMiddleware } from  "./middlewares";
 
 
 
@@ -17,9 +18,10 @@ app.use(logger);
 app.use(express.urlencoded({extended: true}));
 app.use(
   session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({mongoUrl: process.env.DB_URL}),
 })
 );
 
@@ -27,5 +29,6 @@ app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+
 
 export default app;
